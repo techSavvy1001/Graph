43e552ef04db8161b53d2bfb32f4cc82d6c3f670
Karan Raj <kr74747@gmail.com>
2025-07-10 20:20:18 +0530

    1

--- G1.java
+++ G1.java
@@ -1,7 +1,15 @@
 import java.util.*;
 
+// GitHub Repository:
+// git add .
+// git commit -m "Describe your update"
+// git push
+
+
 // Graph using adjacency list representation
 // This is an undirected graph
+
+/*
 
 public class G1{
     static class Edge{
@@ -53,4 +61,53 @@ public class G1{
         
 
     }
+}
+
+*/
+
+
+public class G1{
+    static class Edge{
+        int src;
+        int des;
+        public Edge(int s, int d){
+            this.src  = s;
+            this.des = d;
+        }
+    }
+
+    public static void createGraph(ArrayList<Edge> graph[]){
+        for(int i=0; i < graph.length; i++){
+            graph[i] = new ArrayList<Edge>();
+        }    
+        graph[0].add(new Edge(0, 2));
+        graph[1].add(new Edge(1, 2));   
+        graph[1].add(new Edge(1, 3));
+        graph[2].add(new Edge(2, 0));
+        graph[2].add(new Edge(2, 1));
+        graph[2].add(new Edge(2, 3));
+        graph[3].add(new Edge(3, 1));
+        graph[3].add(new Edge(3, 2));
+
+    }
+
+    @SuppressWarnings("unchecked")
+    // This is the main method where the program starts execution
+    public static void main(String args[]) {
+        int V = 4;
+        ArrayList<Edge> [] graph = new ArrayList[V];
+
+        createGraph(graph);
+
+        // Print the adjacency list representation of the graph
+        for(int i = 0; i < V; i++){
+            for(Edge e: graph[i]){
+                System.out.print(e.src + ": " + e.des + " ");
+            }
+            System.out.println();
+        }
+
+    }
+
+    
 }
